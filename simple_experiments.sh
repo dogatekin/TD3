bc6eab5c60f911fd58df7d900da7a917d39693e5
Doga Tekin <doga.tekin@epfl.ch>
2019-04-02 11:59:53 +0200

    Wrote helpers for visualizations

--- simple_experiments.sh
+++ simple_experiments.sh
@@ -3,7 +3,7 @@
 # Script to reproduce results
 # Original parameters:
 # --start_timesteps 10000
-# --max_timesteps 1e6
+# --max_timesteps 1000000
 
 for ((i=0;i<5;i+=1))
 do 
@@ -12,12 +12,15 @@ do
 	--env_name "Pendulum-v0" \
 	--seed $i \
 	--start_timesteps 1000 \
-	--max_timesteps 50000
+	--max_timesteps 100000
 
 	python main.py \
 	--policy_name "TD3" \
 	--env_name "Pendulum-v0" \
 	--seed $i \
 	--start_timesteps 1000 \
-	--max_timesteps 50000
+	--max_timesteps 100000
 done
+
+# Sleep after experiments are done
+pmset sleepnow
